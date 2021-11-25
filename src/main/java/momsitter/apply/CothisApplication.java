d9c68b70de9c1c4e201c140dcab6733accd21eb9
cothis <minho1030@naver.com>
2021-11-25 19:45:59 +0900

    setup: 기본 스프링부트 프로젝트 셋업

---
+++ src/main/java/momsitter/apply/CothisApplication.java
@@ -0,0 +1,13 @@
+package momsitter.apply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CothisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CothisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/momsitter/apply/CothisApplicationTests.java
@@ -0,0 +1,13 @@
+package momsitter.apply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CothisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
